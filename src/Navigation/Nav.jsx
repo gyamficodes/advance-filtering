e25b869cfb1d415c40e37df1339c709c1dc193dd
Gyamfi John <115163376+gyamficodes@users.noreply.github.com>
2024-04-23 17:41:53 -0700

    Project name changed.

--- src/Navigation/Nav.jsx
+++ src/Navigation/Nav.jsx
@@ -6,20 +6,26 @@ const Nav = () => {
     <>
       <div className=" flex *:items-center   border-b border-bb ml-[2rem] justify-around p-[20px] z-[999]">
         <div>
-          <input placeholder="Search your item" type="text" className=" border-none bg-[#f7f6f6] outline-none mr-[20px] rounded-[5px] relative w-[14rem]  "  />
+          <input
+            placeholder="Search your item"
+            type="text"
+            className=" border-none bg-[#f7f6f6] outline-none mr-[20px] rounded-[5px] relative w-[14rem]  "
+          />
         </div>
         <div className=" pfoile-container">
-             <a href="#" className=" decoration-clone text-icons-c">
-              <FiHeart className="Icons"/>
-             </a>
+          <div className="  flex items-center gap-3 h-full">
+            <a href="#" className=" decoration-clone text-icons-c">
+              <FiHeart className=" w-[1.5rem] h-[1.5rem]  text-[#00]" />
+            </a>
 
-             <a href="#" className=" decoration-clone text-icons-c">
-              <AiOutlineShoppingCart className="Icons"/>
-             </a>
+            <a href="#" className=" decoration-clone text-icons-c">
+              <AiOutlineShoppingCart className="w-[1.5rem] h-[1.5rem] text-[#00]" />
+            </a>
 
-             <a href="#" className=" decoration-clone text-icons-c">
-              <AiOutlineUserAdd className="Icons"/>
-             </a>
+            <a href="#" className=" decoration-clone text-icons-c">
+              <AiOutlineUserAdd className="w-[1.5rem] h-[1.5rem] text-[#00]" />
+            </a>
+          </div>
         </div>
       </div>
     </>
--- src/Products/Product.jsx
+++ src/Products/Product.jsx
@@ -1,9 +1,36 @@
-
-
+import { FaStar } from "react-icons/fa";
+import { BsBagHeartFill } from "react-icons/bs";
 const Product = () => {
   return (
-    <div>Product</div>
-  )
-}
+    <>
+      <section className="card-container">
+        <div className="card">
+          <img
+            src="https://m.media-amazon.com/images/I/61bncQ44yML._AC_UX695_.jpg"
+            alt="Card-image"
+          />
+          <div className=" card-details">
+            <h1 className="card-titile">Shoe</h1>
+            <div className="card-reviews">
+              <div>
+                <FaStar />
+                <FaStar />
+                <FaStar />
+                <FaStar />
+              </div>
+              <span className="total-reviews">4</span>
+            </div>
+            <div className="card-price">
+              <div className="price">
+              <del>$300</del>200
+              </div>
+              <div className="bag"><BsBagHeartFill /></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
 
-export default Product
+export default Product;
